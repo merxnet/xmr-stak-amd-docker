9bd60feb1d311e9fa6949a40e4cd5510e45f56a5
Jonathan Duggan <jonathan@merxnet.io>
2018-08-07 12:26:27 -0400

    must pass AMD device

--- bin/xmr-stak-amd.sh
+++ bin/xmr-stak-amd.sh
@@ -8,12 +8,12 @@ PASSWD=
 CURR=monero7
 
 if [[ -f /etc/xmr-stak/config.txt && -f /etc/xmr-stak/pools.txt && -f /etc/xmr-stak/amd.txt ]]; then
-  docker run -d --rm --name xmr-stak-amd -v /etc/xmr-stak:/etc/xmr-stak:ro merxnet/xmr-stak-amd \
+  docker run -d --rm --name xmr-stak-amd --device /dev/dri -v /etc/xmr-stak:/etc/xmr-stak:ro merxnet/xmr-stak-amd \
     -c /etc/xmr-stak/config.txt \
     -C /etc/xmr-stak/pools.txt \
     --amd /etc/xmr-stak/amd.txt
 else
-  docker run -d --rm --name xmr-stak-amd merxnet/xmr-stak-amd \
+  docker run -d --rm --name xmr-stak-amd --device /dev/dri merxnet/xmr-stak-amd \
     -o ${POOL}
     -u ${WALLET} \
     -p ${PASSWD} \
